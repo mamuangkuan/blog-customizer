a54cccb717c0cc521e1ec4911c27dfabd16b9b5f
Evgenii Iakovina <evpen@yandex.ru>
2024-10-03 21:09:40 +0300

    Разработан функционал сайдбара с опциями

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, useState, CSSProperties } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -12,22 +15,25 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
-//добавляем комментарий для проверки
-
 const App = () => {
+	const [settings, setSettings] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const handleUpdateSettings = (newSettings: ArticleStateType) => {
+		setSettings(newSettings);
+	};
+
+	const customStyles: CSSProperties = {
+		'--font-family': settings.fontFamilyOption.value,
+		'--font-size': settings.fontSizeOption.value,
+		'--font-color': settings.fontColor.value,
+		'--container-width': settings.contentWidth.value,
+		'--bg-color': settings.backgroundColor.value,
+	} as CSSProperties;
+
 	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+		<main className={clsx(styles.main)} style={customStyles}>
+			<ArticleParamsForm onUpdate={handleUpdateSettings} />
 			<Article />
 		</main>
 	);
